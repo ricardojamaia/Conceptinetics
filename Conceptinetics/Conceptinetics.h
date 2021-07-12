771d7f6887508b940b82bfc91c4310922d7f0531
Ricardo Maia <ricardo.ja.maia@gmail.com>
2021-07-12 11:18:38 +0100

    Using serial 1 (USE_DMX_SERIAL_1)

--- Conceptinetics/Conceptinetics.h
+++ Conceptinetics/Conceptinetics.h
@@ -67,8 +67,8 @@
 // Define which serial port to use as DMX port, only one can be 
 // selected at the time by uncommenting one of the following
 // lines
-#define USE_DMX_SERIAL_0
-//#define USE_DMX_SERIAL_1
+//#define USE_DMX_SERIAL_0
+#define USE_DMX_SERIAL_1
 //#define USE_DMX_SERIAL_2
 //#define USE_DMX_SERIAL_3
 
